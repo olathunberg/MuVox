819f99cd2c5fa747c2ef580a471ac631a03749a0
Ola Thunberg <ola.thunberg@gmail.com>
2020-02-23 15:31:06 +0100

    BooleanToVisibilityConverter

--- MuVox.Testing/UI Features/BooleanToVisibilityConverterTests.cs
+++ MuVox.Testing/UI Features/BooleanToVisibilityConverterTests.cs
@@ -1,43 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Windows;
 using TTech.MuVox.UI_Features.Converters;
 
 namespace MuVox.Testing.UI_Features
 {
     [TestClass]
-    public class InverseBooleanToVisibilityConverterTests
+    public class BooleanToVisibilityConverterTests
     {
-        private InverseBooleanToVisibilityConverter converter;
+        private BooleanToVisibilityConverter converter;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            converter = new InverseBooleanToVisibilityConverter();
+            converter = new BooleanToVisibilityConverter();
         }
 
         [TestMethod]
-        public void Convert_Null_Visible()
+        public void Convert_Null_Collapsed()
         {
             var visibility = converter.Convert(null, null, null, null);
 
-            Assert.AreEqual(Visibility.Visible, visibility);
+            Assert.AreEqual(Visibility.Collapsed, visibility);
         }
 
         [TestMethod]
-        public void Convert_False_Visible()
+        public void Convert_False_Collapsed()
         {
             var visibility = converter.Convert(false, null, null, null);
 
-            Assert.AreEqual(Visibility.Visible, visibility);
+            Assert.AreEqual(Visibility.Collapsed, visibility);
         }
 
         [TestMethod]
-        public void Convert_True_Collapsed()
+        public void Convert_True_Visible()
         {
             var visibility = converter.Convert(true, null, null, null);
 
-            Assert.AreEqual(Visibility.Collapsed, visibility);
+            Assert.AreEqual(Visibility.Visible, visibility);
         }
     }
 }
---
+++ MuVox/UI Features/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TTech.MuVox.UI_Features.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            if (value is bool visibility && visibility == true)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
