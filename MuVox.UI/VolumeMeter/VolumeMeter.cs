a12f77fb47bd12a22bb5bf486f9324cbb8d38631
Ola Thunberg <ola.thunberg@gmail.com>
2020-07-29 08:42:29 +0200

    Db based coloring

--- MuVox.UI/VolumeMeter/VolumeMeter.cs
+++ MuVox.UI/VolumeMeter/VolumeMeter.cs
@@ -8,7 +8,19 @@ namespace TTech.MuVox.UI.VolumeMeter
     public class VolumeMeter : FrameworkElement
     {
         private double maxMark;
+        private Brush darkGreen = new SolidColorBrush(Color.FromRgb(0x00, 0x4d, 0x0d));
+        private Brush darkYellow = new SolidColorBrush(Color.FromRgb(0x4d, 0x4d, 0x00));
+        private Brush brightRed = new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00));
+        private Brush brightGreen = new SolidColorBrush(Color.FromRgb(0x00, 0xe6, 0x00));
+        private Brush brightYellow = new SolidColorBrush(Color.FromRgb(0xe6, 0xe6, 0x00));
+        private Brush darkRed = new SolidColorBrush(Color.FromRgb(0x8b, 0x00, 0x00));
+        private double width;
+        private int blockHeight = 5;
+        private int blockSpacing = 2;
+        private int numBlocks;
         private DateTime maxTime = DateTime.Now;
+        private double yellowMark;
+        private double redMark;
 
         public float Amplitude
         {
@@ -49,21 +61,9 @@ namespace TTech.MuVox.UI.VolumeMeter
         /// </summary>
         public int PeakHoldTime { get { return Settings.PeakHoldTime; } }
 
-        private Brush darkGreen = new SolidColorBrush(Color.FromRgb(0x00, 0x4d, 0x0d));
-        private Brush darkOrange = new SolidColorBrush(Color.FromRgb(0x4d, 0x4d, 0x00));
-        private Brush brightRed = new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00));
-        private Brush brightGreen = new SolidColorBrush(Color.FromRgb(0x00, 0xe6, 0x00));
-        private Brush brightOrange = new SolidColorBrush(Color.FromRgb(0xe6, 0xe6, 0x00));
-        private Brush darkRed = new SolidColorBrush(Color.FromRgb(0x8b, 0x00, 0x00));
-        private double width;
-        private int blockHeight = 5;
-        private int blockSpacing = 2;
-        private int numBlocks;
-
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
-            width = this.ActualWidth - 2;
-            numBlocks = (int)((this.ActualHeight + blockSpacing - 1) / blockHeight);
+            Initialize();
         }
 
         protected override void OnRender(DrawingContext drawingContext)
@@ -74,17 +74,10 @@ namespace TTech.MuVox.UI.VolumeMeter
                 return;
             if (numBlocks == 0)
             {
-                numBlocks = (int)((this.ActualHeight + blockSpacing - 1) / blockHeight);
-                width = this.ActualWidth - 2;
-                for (int i = 0; i < numBlocks; i++)
-                {
-                    PaintBlock(drawingContext, i, false);
-                }
+                Initialize();
             }
 
-            var db = DesignerProperties.GetIsInDesignMode(this)
-                ? NAudio.Utils.Decibels.LinearToDecibels(0) //new Random(DateTime.Now.Millisecond).NextDouble())
-                : NAudio.Utils.Decibels.LinearToDecibels(Amplitude);
+            var db = NAudio.Utils.Decibels.LinearToDecibels(Amplitude);
             if (db < MinDb)
                 db = MinDb;
             if (db > MaxDb)
@@ -99,12 +92,24 @@ namespace TTech.MuVox.UI.VolumeMeter
             if ((DateTime.Now - maxTime).TotalMilliseconds > PeakHoldTime && maxMark > MinDb)
                 maxMark -= 2;
 
-            var numBlocksToPaint = ((maxMark - MinDb) / (MaxDb - MinDb)) * numBlocks;
-
+            var numBlocksToPaint = DecibelToBlock(maxMark);
             for (int i = 0; i < numBlocks; i++)
             {
                 PaintBlock(drawingContext, i, i < numBlocksToPaint);
             }
+        }
+
+        private void Initialize()
+        {
+            numBlocks = (int)((this.ActualHeight + blockSpacing - 1) / blockHeight);
+            width = this.ActualWidth - 2;
+            yellowMark = DecibelToBlock(-12);
+            redMark = DecibelToBlock(-3);
+        }
+
+        private double DecibelToBlock(double db)
+        {
+            return ((db - MinDb) / (MaxDb - MinDb)) * numBlocks;
         }
 
         private void PaintBlock(DrawingContext drawingContext, int i, bool isOn)
@@ -113,12 +118,12 @@ namespace TTech.MuVox.UI.VolumeMeter
             {
                 _ when !isOn => i switch
                 {
-                    _ when i < numBlocks - 4 => darkGreen,  // Config
-                    _ when i < numBlocks - 1 => darkOrange, // Config
+                    _ when i < yellowMark => darkGreen,  // Config
+                    _ when i < redMark => darkYellow, // Config
                     _ => darkRed,
                 },
-                _ when i < numBlocks - 4 => brightGreen,  // Config
-                _ when i < numBlocks - 1 => brightOrange, // Config
+                _ when i < yellowMark => brightGreen,  // Config
+                _ when i < redMark => brightYellow, // Config
                 _ => brightRed
             };
 
