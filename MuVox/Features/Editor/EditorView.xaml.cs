b0d019b7edf9e6870d78d84a7f86ea44f0b00412
Ola Thunberg <ola.thunberg@gmail.com>
2020-07-29 08:42:11 +0200

    Added missing files

---
+++ MuVox/Features/Editor/EditorView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace TTech.MuVox.Features.Editor
+{
+    public partial class EditorView : UserControl
+    {
+        public EditorView()
+        {
+            InitializeComponent();
+            this.Loaded += MarkerView_Loaded;
+        }
+
+        void MarkerView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Focus();
+        }
+    }
+}
