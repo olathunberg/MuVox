81129f1775a31c9f4665110c3a7d8f862ef7b757
Ola Thunberg <ola.thunberg@gmail.com>
2020-08-06 23:00:42 +0200

    MultiTrack UI

---
+++ MuVox.MultiTrack/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MuVox.MultiTrack
+{
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            //services.AddSingleton<MainWindow>();
+        }
+    }
+}
---
+++ MuVox.MultiTrack/Fader/Fader.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MuVox.MultiTrack.Fader
+{
+    /// <summary>
+    /// Interaction logic for Fader.xaml
+    /// </summary>
+    public partial class Fader : UserControl
+    {
+        public Fader()
+        {
+            InitializeComponent();
+        }
+
+        public float Amplitude
+        {
+            get { return (float)GetValue(AmplitudeProperty); }
+            set { SetValue(AmplitudeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Amplitude.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AmplitudeProperty =
+            DependencyProperty.Register(nameof(Amplitude), typeof(float), typeof(Fader), new PropertyMetadata(0f, (s, e) =>
+            {
+                if (s is Fader fader)
+                {
+                    fader.Meter.Amplitude = (float)e.NewValue;
+                }
+                if (s is FrameworkElement frameworkElement)
+                    frameworkElement.InvalidateVisual();
+            }));
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(Fader), new PropertyMetadata("Track x"));
+    }
+}
